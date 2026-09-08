62bf2ead27ce49701df5d0ff731304b472a6380c
aliburakakbass <aliburakakbas560@gmail.com>
2026-09-08 13:37:35 +0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
